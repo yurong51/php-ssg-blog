9b18c4dc3c10aec20cefc3c878b0b23bed1c9ba0
yuri-song <sy120003@gmail.com>
2021-01-12 15:56:50 +0900

    1.12

--- common.js
+++ common.js
@@ -1,14 +1,13 @@
 console.clear();
 
 function MobileTopbar__init() {
-  $('.mobile-top-bar__btn-toggle-side-bar').click(function(){
+  $('.mobile-top-bar__btn-toggle-side-bar').click(function () {
     let $this = $(this);
-    
-    if ( $this.hasClass('active')) {
+
+    if ($this.hasClass('active')) {
       $this.removeClass('active');
       $('.mobile-side-bar').removeClass('active');
-    }
-    else {
+    } else {
       $this.addClass('active');
       $('.mobile-side-bar').addClass('active');
     }
@@ -16,3 +15,77 @@ function MobileTopbar__init() {
 }
 
 MobileTopbar__init();
+
+/* 토스트 UI 시작 */
+// 유튜브 플러그인 시작
+function youtubePlugin() {
+  toastui.Editor.codeBlockManager.setReplacer('youtube', youtubeId => {
+    // Indentify multiple code blocks
+    const wrapperId = `yt${Math.random().toString(36).substr(2, 10)}`;
+
+    // Avoid sanitizing iframe tag
+    setTimeout(renderYoutube.bind(null, wrapperId, youtubeId), 0);
+
+    return `<div id="${wrapperId}"></div>`;
+  });
+}
+
+function renderYoutube(wrapperId, youtubeId) {
+  const el = document.querySelector(`#${wrapperId}`);
+
+  el.innerHTML = `<div class="toast-ui-youtube-plugin-wrap"><iframe src="https://www.youtube.com/embed/${youtubeId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
+}
+// 유튜브 플러그인 끝
+
+// codepen 플러그인 시작
+function codepenPlugin() {
+  toastui.Editor.codeBlockManager.setReplacer('codepen', url => {
+    const wrapperId = `yt${Math.random().toString(36).substr(2, 10)}`;
+
+    // Avoid sanitizing iframe tag
+    setTimeout(renderCodepen.bind(null, wrapperId, url), 0);
+
+    return `<div id="${wrapperId}"></div>`;
+  });
+}
+
+function renderCodepen(wrapperId, url) {
+  const el = document.querySelector(`#${wrapperId}`);
+
+  var urlParams = new URLSearchParams(url.split('?')[1]);
+  var height = urlParams.get('height');
+
+  el.innerHTML = `<div class="toast-ui-codepen-plugin-wrap"><iframe height="${height}" scrolling="no" src="${url}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe></div>`;
+}
+// codepen 플러그인 끝
+
+function Editor__init() {
+  $('.toast-ui-editor').each(function (index, node) {
+    var initialValue = $(node).prev().html().trim().replace(/<!--REPLACE:script-->/gi, 'script');
+
+    var editor = new toastui.Editor({
+      el: node,
+      previewStyle: 'vertical',
+      initialValue: initialValue,
+      height: 600,
+      plugins: [toastui.Editor.plugin.codeSyntaxHighlight, youtubePlugin, codepenPlugin]
+    });
+  });
+}
+Editor__init();
+
+function EditorViewer__init() {
+  $('.toast-ui-viewer').each(function (index, node) {
+    var initialValue = $(node).prev().html().trim().replace(/<!--REPLACE:script-->/gi, 'script');
+    var viewer = new toastui.Editor.factory({
+      el: node,
+      initialValue: initialValue,
+      viewer: true,
+      plugins: [toastui.Editor.plugin.codeSyntaxHighlight, youtubePlugin, codepenPlugin]
+    });
+  });
+}
+EditorViewer__init();
+
+/* 토스트 UI 끝 */
+s
